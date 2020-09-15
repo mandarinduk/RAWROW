86264d533bdbc85d16b62a222cfa89af1a4fbca1
realryankim <ryankimofficial@gmail.com>
2020-09-15 14:00:35 +0900

    [12기 김상준] Add: 로그인 페이지 레이아웃

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,91 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import "./Login.scss";
 
 class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
+  constructor() {
+    super();
+    this.state = {
+      memberId: "",
+      memberPasswd: "",
+    }
   }
+
+  handleInput = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleClick = (e) => {
+    const { memberId, memberPasswd} = this.state
+    if( memberId.length < 1) {
+      alert("아이디 항목은 필수 입력값입니다.")
+    } else if ( memberPasswd.length < 4) {
+      alert("패스워드 항목이 4자(개) 이상으로 해주십시오.")
+    }
+  }
+
+  enterValue = (e) => {
+    const { memberId, memberPasswd} = this.state
+    if (e.key === "Enter") {
+      if( memberId.length < 1) {
+        alert("아이디 항목은 필수 입력값입니다.")
+      } else if ( memberPasswd.length < 4) {
+        alert("패스워드 항목이 4자(개) 이상으로 해주십시오.")
+      }
+    }
+  }
+  
+
+  render() {
+    return (
+    <div>
+      <div className="loginTitle">
+        <div className="titleAnimation">
+          <h2>LOGIN</h2>
+        </div>
+        <div className="login">
+          <article>
+            <div className="loginBox first">
+              <span className="loginBoxTitle">아이디</span>
+              <label className="loginBoxDesc">
+                <input onKeyUp={this.enterValue} onChange={this.handleInput} id="memberId" name="memberId" type="text" placeholder="아이디를 입력해주세요."></input>
+              </label>
+            </div>
+            <div className="loginBox">
+              <span className="loginBoxTitle">비밀번호</span>
+              <label className="loginBoxDesc">
+                <input onKeyUp={this.enterValue} onChange={this.handleInput} id="memberPasswd" name="memberPasswd" type="text" placeholder="비밀번호를 입력해주세요."></input>
+              </label>
+            </div>
+            <a href="#none"><button onClick={this.handleClick} className="loginBtnBlack">로그인</button></a>
+            <a href="#none"><button onClick={this.handleClick} className="loginBtnWhite">비회원 주문조회</button></a>
+            <ul className="snsArea">
+              <li>
+                <a href="#none">
+                  <span className="snsBg"><img src="https://rawrow.com/web/upload/mundane/login_naver.png" alt="네이버계정 로그인" /></span>
+                  <span className="snsText">naver login</span>
+                </a>
+              </li>
+              <li>
+                <a href="#none">
+                  <span className="snsBg"><img src="https://rawrow.com/web/upload/mundane/login_kakao.png" alt="카카오계정 로그인"/></span>
+                  <span className="snsText">kakao login</span>
+                </a>
+              </li>
+            </ul>
+            <ul className="button">
+              <li><Link to="#none">아이디 찾기</Link></li>
+              <li><Link to="#none">비밀번호 찾기</Link></li>
+              <li><a href="/signup">회원 가입</a></li>
+            </ul>
+          </article>
+        </div>
+      </div>
+    </div>
+  )}
 }
 
 export default Login;
